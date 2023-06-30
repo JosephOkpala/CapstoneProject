2bdf73718d445d56627d1012a8576f8f43e278fd
JosephOkpala <okpalajoseph995@gmail.com>
2023-07-01 00:44:03 +0100

    qrcode

--- src/pages/Qrcode.tsx
+++ src/pages/Qrcode.tsx
@@ -1,7 +1,62 @@
-const Qrcode = () => {
+import React, { useState, useEffect } from 'react';
+import QRCode from 'qrcode.react';
+
+export interface IQrcodeProps {}
+
+const Qrcode = (props: IQrcodeProps) => {
+  const [url, setUrl] = useState('');
+  const [qrCodes, setQrCodes] = useState<string[]>([]);
+
+  useEffect(() => {
+    const storedQrCodes = localStorage.getItem('qrCodes');
+    if (storedQrCodes) {
+      setQrCodes(JSON.parse(storedQrCodes));
+    }
+  }, []);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const newQrCodes = [...qrCodes, url];
+    setQrCodes(newQrCodes);
+    localStorage.setItem('qrCodes', JSON.stringify(newQrCodes));
+    setUrl('');
+  };
+
   return (
-    <div>
-      <h1>Qrcode</h1>
+    <div className="qrCodeContainer">
+      <div className="formDiv">
+        <form onSubmit={handleSubmit}>
+          <h1>Enter Your Link</h1>
+          <input
+            className=""
+            type="text"
+            placeholder="enter your link"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="generateBtn loginBtn"
+            style={{ fontSize: '1.2rem' }}
+          >
+            Generate QR Code
+          </button>
+        </form>
+      </div>
+
+      <div className="qrCode">
+        {qrCodes.map((qrCode, index) => (
+          <div key={index} className="">
+            <QRCode
+              id={`qrCodeId-${index}`}
+              value={qrCode}
+              bgColor="white"
+              fgColor="black"
+              level="H"
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
