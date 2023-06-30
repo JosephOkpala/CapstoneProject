07368acea0b25b45ce77a25fb0f4c7a19daa76b7
JosephOkpala <okpalajoseph995@gmail.com>
2023-06-30 23:14:36 +0100

    updated imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import MyUrls from './pages/MyUrls';
 import FAQs from './pages/FAQs';
-import Analytics from './pages/Analytics';
 import Pricing from './pages/Pricing';
 import Features from './pages/Features';
 import Navbar from './Navbar';
@@ -23,7 +22,6 @@ function App() {
           <Route path="/" element={<MyUrls />} />
           <Route path="/features" element={<Features />} />
           <Route path="/pricing" element={<Pricing />} />
-          <Route path="/analytics" element={<Analytics />} />
           <Route path="/faqs" element={<FAQs />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
