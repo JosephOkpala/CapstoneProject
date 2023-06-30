526069af0c97ad5ecdc84f97fe9c40d0cb818d07
JosephOkpala <okpalajoseph995@gmail.com>
2023-06-30 17:56:24 +0100

    updated the imports

--- src/pages/MyUrls.tsx
+++ src/pages/MyUrls.tsx
@@ -1,32 +1,43 @@
 import { Link } from 'react-router-dom';
 import '../styles/myurls.css';
+import Features from './Features';
+import Pricing from './Pricing';
+import Analytics from './Analytics';
+import FAQs from './FAQs';
+import Footer from './Footer';
 
 const MyUrls = () => {
   return (
-    <div className="hero">
-      <div className="hero-div">
-        <div className="hero-text">
-          <h1 className="hero-header">
-            Optimize Your Online Experience With Our
-            <span>
-              Advanced
-              <span className="blue-herotext"> URL Shortening</span> Solution
-            </span>
-          </h1>
-          <p>
-            Personalize your shortened URLs to align with your brand identity.
-            Utilize custom slugs and branded links and domain customization
-            options to reinforce your brand persence and enhance user
-            engagement.
-          </p>
-        </div>
-        <div>
-          <Link to="/signup">
-            <button className="btn-header1">Sign up</button>
-          </Link>
-          <button className="btn-header2">Learn more</button>
+    <div>
+      <div className="hero">
+        <div className="hero-div">
+          <div className="hero-text">
+            <h1 className="hero-header">
+              Optimize Your Online Experience With Our
+              <span>
+                Advanced
+                <span className="blue-herotext"> URL Shortening</span> Solution
+              </span>
+            </h1>
+            <p>
+              Personalize your shortened URLs to align with your brand identity.
+              Utilize custom slugs and branded links and domain customization
+              options to reinforce your brand persence and enhance user
+              engagement.
+            </p>
+          </div>
+          <div>
+            <Link to="/signup">
+              <button className="btn-header1">Sign up</button>
+            </Link>
+            <button className="btn-header2">Learn more</button>
+          </div>
         </div>
       </div>
+      <Features />
+      <Pricing />
+      <FAQs />
+      <Footer />
     </div>
   );
 };
