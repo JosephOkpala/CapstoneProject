df000970d0a42de9ac8bcdec9a4613489ce67404
JosephOkpala <okpalajoseph995@gmail.com>
2023-06-30 17:52:05 +0100

    added outlet, nested links

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
 import { UserAuth } from '../context/AuthContext';
+import '../styles/dashboard.css';
 
 const Dashboard = () => {
   const { user } = UserAuth();
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <h2>Welcome {user?.displayName}!</h2>
+    <div className="dashboard">
+      <div className="sideBar">
+        <div className="userProfile">
+          <img src={user?.photoURL} alt="" />
+          <h3>{user?.displayName}</h3>
+          <p>{user?.email}</p>
+        </div>
+        <div className="sideBarLinks">
+          <div>
+            <Link to="add">
+              <h2>Add</h2>
+            </Link>
+          </div>
+          <div>
+            <Link to="qrcode">
+              <h2>QrCode</h2>
+            </Link>
+          </div>
+          <div>
+            <Link to="qrcode">
+              <h2>Analytics</h2>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="mainDiv">
+        <h1>Welcome back {user?.displayName}</h1>
+        <Outlet />
+      </div>
     </div>
   );
 };
